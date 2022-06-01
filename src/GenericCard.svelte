3a85ccd96ef8d415cb5c55a8a382e4ba9f76cf9a
Nilesh <19304+nileshtrivedi@users.noreply.github.com>
2022-06-01 20:52:43 +0100

    Added TypeScript, misc fixes

--- src/GenericCard.svelte
+++ src/GenericCard.svelte
@@ -1,6 +1,5 @@
-<script>
-  import Icon from "./tailwindui/Icon.svelte"
-  export let item;
+<script lang="ts">
+  export let item: {name: string, creators: string};
 </script>
 
 <a class="flex flex-wrap p-8 justify-between rounded-lg break-inside-avoid w-64 max-w-sm bg-lightPrimCont text-lightPrimary dark:bg-darkPrimCont dark:text-darkPrimary hover:bg-lightPrimary hover:bg-darkPrimary" href="#/item/{item.rowid}">
--- src/Review.svelte
+++ src/Review.svelte
@@ -1,5 +1,12 @@
-<script>
-    
+<script lang="ts">
+    export let review: any;
 </script>
 
-review
+<article class="px-3 py-4 bg-lightSecondary2 text-lightSecondary1 dark:bg-darkSecondary2 dark:text-darkSecondary1 rounded-lg text-xs w-48 max-w-sm shrink-0">
+    <h3 class="font-semibold">{review.blurb.toString().slice(0,10)}...</h3>
+    <p class="mt-2 line-clamp">{review.blurb}</p>
+    <div class="mt-3">
+        <sl-rating value={review.rating}></sl-rating>
+        <span class="ml-2">...{review.by}</span>
+    </div>
+</article>
