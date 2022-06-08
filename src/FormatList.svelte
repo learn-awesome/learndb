fb051624cd83d1468126e775c42cfbfabe13972e
Nilesh <19304+nileshtrivedi@users.noreply.github.com>
2022-06-08 10:54:44 +0100

    FormatList layout fix

--- src/FormatList.svelte
+++ src/FormatList.svelte
@@ -3,13 +3,13 @@
   
 </script>
 
-<div class=" mx-auto flex flex-wrap gap-5 justify-center items-center">
+<div class=" mx-auto flex flex-wrap gap-3 justify-center items-center">
   {#each formats as format}
-  <a href="#/format/{format.id}" class="w-52 sm:w-64 flex flex-col rounded-lg shadow overflow-hidden transform transition ease-out duration-300 hover:scale-105 ">
+  <a href="#/format/{format.id}" class="w-52 flex flex-col rounded-lg shadow-lg overflow-hidden transform transition ease-out duration-300 hover:scale-105 ">
       <div class="flex-shrink-0">
-        <img class="h-48 w-full object-cover" src={format.image} alt="">
+        <img class="h-32 w-full object-cover" src={format.image} alt="">
       </div>
-      <h1 class="text-lg font-semibold p-2 tracking-wider text-primary">{format.name}</h1>
+      <h1 class="text-sm font-semibold p-2 tracking-wider text-primary">{format.name}</h1>
   </a>
   {/each}
 </div>
