5ec8008468888725750faf35c5f91b24332955ea
inclusive-ml <code@inclusive-ml.com>
2022-06-25 13:49:52 +0200

    ops: moved components in packages

---
+++ src/packages/topic-list/TopicList.svelte
@@ -0,0 +1,7 @@
+<script>
+  import TopicMasonryGrid from '../topic-gallery/TopicMasonryGrid.svelte';
+  export let alltopics;
+  import SearchForm from '../search-form/SearchForm.svelte';
+</script>
+
+<TopicMasonryGrid {alltopics} />
