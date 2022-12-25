13cb93bc0815a6a64fffb250042f8b4085fa6653
Nilesh <git@nileshtrivedi.com>
2022-12-25 23:18:07 +0000

    Generating alltopics.json for the mapview at build time

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+import { topics } from './db/topics.js'
+import fs from 'fs';
+
+function generateTopicJSON(){
+	console.log("Generating public/alltopics.json ...")
+	const topics_db = topics.trimStart().trimEnd().split('\n').map(j => JSON.parse(j));
+	try {
+		fs.writeFileSync('public/alltopics.json', JSON.stringify(topics_db));
+	} catch (error) {
+		console.error(error);
+	}
+
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [generateTopicJSON(),svelte()],
 })
