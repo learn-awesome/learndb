b7e4ba611c1914c7d8ed9d9d5c8d7f701a20f66c
Huda Joad <hudajoad@gmail.com>
2023-11-24 17:50:59 +0300

    trying with axios instead of node-fetch

--- netlify/functions/handleMetadata.js
+++ netlify/functions/handleMetadata.js
@@ -1,5 +1,5 @@
 const https = require('https'); // Import for webscraping (fetchContentFromURL(url) function
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 // Placeholder function to fetch content from URL using a web scraping service
 async function fetchContentFromURL(url) {
