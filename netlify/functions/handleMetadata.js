a4da9cbf3f2e4800d384daae9b1d88199e8b6a90
Huda Joad <hudajoad@gmail.com>
2023-11-24 17:10:48 +0300

    changed the format back

--- netlify/functions/handleMetadata.js
+++ netlify/functions/handleMetadata.js
@@ -104,7 +104,7 @@ export async function handler(event) {
         // Return the formatted response
         return {
             statusCode: 200,
-            body: fetchedContent,
+            body: JSON.stringify(fetchedContent),
             // body: JSON.stringify(fetchedContent),
         };
     } catch (error) {
