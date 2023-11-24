90e0f024406dbd1a58324f67119cf0330ca8dc1a
Huda Joad <hudajoad@gmail.com>
2023-11-24 18:33:46 +0300

    request node-fetch

--- netlify/functions/handleMetadata.js
+++ netlify/functions/handleMetadata.js
@@ -1,7 +1,7 @@
 const https = require('https'); // Import for webscraping (fetchContentFromURL(url) function
 // const axios = require('axios');
-const fetch = require('node-fetch').default;
-// const fetch = request('node-fetch').default;
+// const fetch = require('node-fetch').default;
+const fetch = request('node-fetch').default;
 
 // Placeholder function to fetch content from URL using a web scraping service
 async function fetchContentFromURL(url) {
