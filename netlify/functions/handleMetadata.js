7e38977885b61c941c822c66a2423a7607614ed8
Maria-Aidarus <maidarus@andrew.cmu.edu>
2023-11-24 20:40:49 +0300

    updated the package.json file

--- netlify/functions/handleMetadata.js
+++ netlify/functions/handleMetadata.js
@@ -1,6 +1,7 @@
 const https = require('https'); // Import for webscraping (fetchContentFromURL(url) function
 // const fetch = require('node-fetch');
-const { OpenAIApi, Configuration } = require('openai');
+// const { OpenAIApi, Configuration } = require('openai');
+import { OpenAIApi, Configuration } from 'openai';
 
 // Placeholder function to fetch content from URL using a web scraping service
 async function fetchContentFromURL(url) {
