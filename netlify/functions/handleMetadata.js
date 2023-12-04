d9735112c5769fa0b5f45ffb8fefcc82ffc23a24
Huda Joad <hudajoad@gmail.com>
2023-12-04 23:48:24 +0300

    preserve body tag

--- netlify/functions/handleMetadata.js
+++ netlify/functions/handleMetadata.js
@@ -20,7 +20,7 @@ function simplifyContent(content) {
     // Preserve the title tag and its content
     let title = content.match(/<title.*?>(.*?)<\/title>/i);
     title = title ? title[1] : '';
-    // Preserve the title tag and its content
+    // Preserve the body tag and its content
     let body = content.match(/<body.*?>(.*?)<\/body>/i);
     body = body ? body[1] : '';
 
